bccc7883d694b0c65b610fb68d9cbb0a11e2fc75
emmacomtois <44363941+emmacomtois@users.noreply.github.com>
2019-02-20 02:10:57 -0500

    Add files via upload

--- custom.js
+++ custom.js
@@ -3,10 +3,29 @@ $(document).ready(function() {
 		$('.collapse').collapse('toggle');
 
 		const text = $(this).find("span").text();
-		if (text === "+ View Resume") {
-			$(this).find("span").text("- Close Resume");
+		if (text === "+ VIEW RESUME") {
+			$(this).find("span").text("- CLOSE RESUME");
 		} else {
-			$(this).find("span").text("+ View Resume");
+			$(this).find("span").text("+ VIEW RESUME");
 		};
+	}); // click
+}); // ready
+
+var initialSrc = "images/index/portrait.png";
+var scrollSrc = "images/index/logo.png";
+
+$(window).scroll(function() {
+   var value = $(this).scrollTop();
+   if (value > 100)
+      $(".logo-index").attr("src", scrollSrc);
+   else
+      $(".logo-index").attr("src", initialSrc);
+});
+
+$(document).ready(function() {
+	$("#menuBtn").click(function(event) {
+		event.preventDefault();
+		$(".nav-col").toggleClass("d-none d-md-block");
+		$(this).find("i").toggleClass("fa-bars").toggleClass("fa-times");
 	}); // click
 }); // ready
